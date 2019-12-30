a3def75279248663e1da6b1da91c99eae42be78d
JustTal <justtal@protonmail.com>
2019-12-29 20:40:31 -0600

    skill command

--- src/main/java/com/civiledcode/mcmmo/commands/SkillCommand.java
+++ src/main/java/com/civiledcode/mcmmo/commands/SkillCommand.java
@@ -16,37 +16,41 @@ public class SkillCommand extends Command {
 
     @Override
     public boolean execute(CommandSender sender, String string, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            PlayerDatabase database = new PlayerDatabase(player);
-            switch (args[0]) {
-                case "mining": {
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("statistics").replace("{TYPE}", "Mining")));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("level")) + database.getLevel(database.TYPE_MINE));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("experience")) + database.getExperience(database.TYPE_MINE));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("experienceNeeded")) + database.getExperienceNeededForNextLevel(database.TYPE_MINE));
-                    break;
-                }
-                case "combat": {
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("combatStatistics").replace("{TYPE}", "Combat")));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("level")) + database.getLevel(database.TYPE_COMBAT));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("experience")) + database.getExperience(database.TYPE_COMBAT));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("experienceNeeded")) + database.getExperienceNeededForNextLevel(database.TYPE_COMBAT));
-                    break;
-                }
-                case "farming": {
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("farmingStatistics").replace("{TYPE}", "Farming")));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("level")) + database.getLevel(database.TYPE_FARMING));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("experience")) + database.getExperience(database.TYPE_FARMING));
-                    player.sendMessage(TextFormat.colorize(Main.lang.getString("experienceNeeded")) + database.getExperienceNeededForNextLevel(database.TYPE_FARMING));
-                    break;
-                }
-                default: {
-                    return false;
+        try {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                PlayerDatabase database = new PlayerDatabase(player);
+                switch (args[0]) {
+                    case "mining": {
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("statistics").replace("{TYPE}", "Mining")));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("level")) + database.getLevel(database.TYPE_MINE));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("experience")) + database.getExperience(database.TYPE_MINE));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("experienceNeeded")) + database.getExperienceNeededForNextLevel(database.TYPE_MINE));
+                        break;
+                    }
+                    case "combat": {
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("combatStatistics").replace("{TYPE}", "Combat")));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("level")) + database.getLevel(database.TYPE_COMBAT));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("experience")) + database.getExperience(database.TYPE_COMBAT));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("experienceNeeded")) + database.getExperienceNeededForNextLevel(database.TYPE_COMBAT));
+                        break;
+                    }
+                    case "farming": {
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("farmingStatistics").replace("{TYPE}", "Farming")));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("level")) + database.getLevel(database.TYPE_FARMING));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("experience")) + database.getExperience(database.TYPE_FARMING));
+                        player.sendMessage(TextFormat.colorize(Main.lang.getString("experienceNeeded")) + database.getExperienceNeededForNextLevel(database.TYPE_FARMING));
+                        break;
+                    }
+                    default: {
+                        return false;
+                    }
                 }
             }
+            return true;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
         }
-        return true;
     }
 
 }
