a6bc000a43d7f2e6d0be7f265b633d3b6961a77e
Civiled <civiledgamer@gmail.com>
2019-12-29 20:11:18 -0800

    Fixed Experience issue with no increase

--- src/main/java/com/civiledcode/mcmmo/events/PlayerJoin.java
+++ src/main/java/com/civiledcode/mcmmo/events/PlayerJoin.java
@@ -11,19 +11,18 @@ public class PlayerJoin implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-            Main.getPlayerDatabase().executeUpdate("INSERT INTO IF NOT EXISTS players (name,experienceMine,experienceCombat,experienceFarming,levelMine,levelCombat,levelFarming) VALUES (\n" +
-                    "  '" + event.getPlayer().getName() + "',\n" +
-                    "  '0',\n" +
-                    "  '0',\n" +
-                    "  '0',\n" +
-                    "  '1',\n" +
-                    "  '1',\n" +
-                    "  '1'\n" +
-                    ");");
             try {
-                Main.getInstance().getLogger().info(Main.getPlayerDatabase().executeSelect("SELECT * FROM players WHERE name='" + event.getPlayer().getName() + "'").getInt("levelMine") + "");
+                Main.getInstance().getLogger().info(Main.getPlayerDatabase().executeSelect("SELECT * FROM players WHERE name='" + event.getPlayer().getName() + "'").getString("name"));
             } catch(SQLException e) {
-                e.printStackTrace();
+                Main.getPlayerDatabase().executeUpdate("INSERT INTO IF NOT EXISTS players (name,experienceMine,experienceCombat,experienceFarming,levelMine,levelCombat,levelFarming) VALUES (\n" +
+                        "  '" + event.getPlayer().getName() + "',\n" +
+                        "  '0',\n" +
+                        "  '0',\n" +
+                        "  '0',\n" +
+                        "  '1',\n" +
+                        "  '1',\n" +
+                        "  '1'\n" +
+                        ");");
             }
     }
 
