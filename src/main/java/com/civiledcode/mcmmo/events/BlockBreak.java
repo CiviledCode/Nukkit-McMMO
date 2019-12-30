cf31ca3704af7fd9d303c0e33d2739201d619519
JustTal <justtal@protonmail.com>
2019-12-30 13:40:17 -0600

    better ore higher xp

--- src/main/java/com/civiledcode/mcmmo/events/BlockBreak.java
+++ src/main/java/com/civiledcode/mcmmo/events/BlockBreak.java
@@ -1,8 +1,7 @@
 package com.civiledcode.mcmmo.events;
 
 import cn.nukkit.Player;
-import cn.nukkit.block.Block;
-import cn.nukkit.block.BlockStone;
+import cn.nukkit.block.*;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockBreakEvent;
@@ -14,10 +13,20 @@ public class BlockBreak implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
         Block block = event.getBlock();
+        Player player = event.getPlayer();
+        PlayerDatabase database = new PlayerDatabase(player);
         if (block instanceof BlockStone) {
-            Player player = event.getPlayer();
-            PlayerDatabase database = new PlayerDatabase(player);
             database.addExperience(Main.cfg.getInt("xpEarnedByMine"), "Mine");
+        } else if (block instanceof BlockOreCoal) {
+            database.addExperience(Main.cfg.getInt("xpEarnedByMine") * 2, "Mine");
+        } else if (block instanceof BlockOreIron) {
+            database.addExperience(Main.cfg.getInt("xpEarnedByMine") * 3, "Mine");
+        } else if (block instanceof BlockOreGold) {
+            database.addExperience(Main.cfg.getInt("xpEarnedByMine") * 4, "Mine");
+        } else if (block instanceof BlockOreDiamond) {
+            database.addExperience(Main.cfg.getInt("xpEarnedByMine") * 5, "Mine");
+        } else if (block instanceof BlockOreEmerald) {
+            database.addExperience(Main.cfg.getInt("xpEarnedByMine") * 6, "Mine");
         }
     }
 
