dd65b5ca11e83a705e9710aa95b7742af33b08d6
JustTal <justtal@protonmail.com>
2019-12-30 02:23:43 -0600

    fix triggers

--- src/main/java/com/civiledcode/mcmmo/events/CheckRewardsEvent.java
+++ src/main/java/com/civiledcode/mcmmo/events/CheckRewardsEvent.java
@@ -3,6 +3,7 @@ package com.civiledcode.mcmmo.events;
 import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.utils.Config;
 import cn.nukkit.utils.ConfigSection;
 import cn.nukkit.utils.TextFormat;
 import com.civiledcode.mcmmo.Main;
@@ -15,18 +16,28 @@ public class CheckRewardsEvent implements Listener {
         Player player = event.getPlayer();
         int level = event.getLevel() + 1;
         String type = event.getType();
-        ConfigSection config;
-        try {
-            config = Main.cfg.getSection("triggers." + type.toLowerCase() + "." + level);
-        } catch (NullPointerException ignored) {
-            return;
+        Config config = Main.cfg;
+        ConfigSection cs = config.getSection(type.toLowerCase().replace("mine", "mining"));
+        String levelCommand = getLevelCommand(cs.toString(), level);
+        String tipCommand = getTipCommand(cs.toString() + "!!!", level);
+        if (levelCommand != null && tipCommand != null) {
+            if (levelCommand.length() > 2) Main.getInstance().getServer().dispatchCommand(Main.getInstance().getServer().getConsoleSender(), levelCommand.replace("{PLAYER}", player.getName()).replace("{PERM}", "999999"));
+            if (tipCommand.length() > 2) player.sendActionBar(TextFormat.colorize(tipCommand));
         }
-        if (config != null) {
-            String command = config.getString("command");
-            String tip = config.getString("tip");
-            if (command.length() > 2) Main.getInstance().getServer().dispatchCommand(Main.getInstance().getServer().getConsoleSender(), command.replace("{PLAYER}", player.getName()).replace("{PERM}", "999999"));
-            if (tip.length() > 2) player.sendTitle(TextFormat.colorize(tip));
-        }
+    }
+
+    public static String getLevelCommand(String cs, int level) {
+        if(!cs.contains("{" + level + "={command=")) return null;
+        String splitTag = cs.split("\\{" + level + "=\\{command=")[1];
+        return splitTag.split(", tip=")[0];
+    }
+
+    public static String getTipCommand(String cs, int level) {
+        if(!cs.contains("{" + level + "={command=")) return null;
+        String splitTag = cs.split("\\{" + level + "=\\{command=")[1];
+        String levelInfo = splitTag.split("!!!")[0];
+        String levelTip = levelInfo.split("tip=")[1];
+        return levelTip.split("}}")[0];
     }
 
 }
