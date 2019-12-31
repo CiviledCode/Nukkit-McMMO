9e75555b0ef94c5fdc63a753010b14d25abcbb80
JustTal <justtal@protonmail.com>
2019-12-31 12:53:52 -0600

    throw sql exception since everything works fine

--- src/main/java/com/civiledcode/mcmmo/events/PlayerJoin.java
+++ src/main/java/com/civiledcode/mcmmo/events/PlayerJoin.java
@@ -5,10 +5,12 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import com.civiledcode.mcmmo.Main;
 
+import java.sql.SQLException;
+
 public class PlayerJoin implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
+    public void onJoin(PlayerJoinEvent event) throws SQLException {
         if(Main.getPlayerDatabase().getInt("SELECT * FROM players WHERE name='" + event.getPlayer().getName() + "'", "levelMine") == -1) {
                 Main.getPlayerDatabase().executeUpdate("INSERT INTO players (name,experienceAcrobatics,experienceArchery,experienceExcavation,experienceFarming,experienceMining,experienceSwords,experienceUnarmed,experienceWoodcutting,levelAcrobatics,levelArchery,levelExcavation,levelFarming,levelMining,levelSwords,levelUnarmed,levelWoodcutting) VALUES (\n" +
                         "  '" + event.getPlayer().getName() + "',\n" +
